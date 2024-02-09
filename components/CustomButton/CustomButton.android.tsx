f0a01ec1ec9cb4bc0d5cdef4a0b7ec545f646b5b
Lawrence <97756376+lawrdev@users.noreply.github.com>
2024-02-09 09:41:11 +0100

    cross-platform buttons

---
+++ components/CustomButton/CustomButton.android.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Pressable, Text } from "react-native";
+
+const CustomButton = ({
+  onPress,
+  title,
+}: {
+  onPress: () => void;
+  title: string;
+}) => {
+  return (
+    <Pressable
+      onPress={onPress}
+      style={{
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "green",
+        borderRadius: 5,
+        paddingHorizontal: 10,
+        paddingVertical: 3,
+      }}
+    >
+      <Text
+        style={{
+          color: "white",
+          fontWeight: "500",
+        }}
+      >
+        {title}
+      </Text>
+    </Pressable>
+  );
+};
+
+export default CustomButton;
