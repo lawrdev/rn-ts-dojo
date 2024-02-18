cc0ff45e5762d88475146f161a0cbf86a76756aa
Lawrence <97756376+lawrdev@users.noreply.github.com>
2024-02-18 20:51:54 +0100

    chore: for polemon card

--- App.tsx
+++ App.tsx
@@ -16,6 +16,7 @@ import {
   useWindowDimensions,
   SafeAreaView,
   Platform,
+  FlatList,
 } from "react-native";
 import CustomButton from "./components/CustomButton/CustomButton";
 import PokemonCard from "./components/PokemonCard";
@@ -23,8 +24,8 @@ const localImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   const chars = useMemo(() => {
-    const obj = {
-      charmander: {
+    const arr = [
+      {
         name: "Charmander",
         image: require("./assets/charmander.png"),
         type: "Fire",
@@ -32,7 +33,7 @@ export default function App() {
         moves: ["Scratch", "Ember", "Growl", "Leer"],
         weaknesses: ["Water", "Rock"],
       },
-      squirtleData: {
+      {
         name: "Squirtle",
         image: require("./assets/squirtle.png"), // Replace with the actual image path
         type: "Water",
@@ -40,7 +41,7 @@ export default function App() {
         moves: ["Tackle", "Water Gun", "Tail Whip", "Withdraw"],
         weaknesses: ["Electric", "Grass"],
       },
-      bulbasaurData: {
+      {
         name: "Bulbasaur",
         image: require("./assets/bulbasaur.png"), // Replace with the actual image path
         type: "Grass",
@@ -48,7 +49,7 @@ export default function App() {
         moves: ["Tackle", "Vine Whip", "Growl", "Leech Seed"],
         weaknesses: ["Fire", "Ice", "Flying", "Psychic"],
       },
-      pikachuData: {
+      {
         name: "Pikachu",
         image: require("./assets/pikachu.png"), // Replace with the actual image path
         type: "Electric",
@@ -56,19 +57,222 @@ export default function App() {
         moves: ["Quick Attack", "Thunderbolt", "Tail Whip", "Growl"],
         weaknesses: ["Ground"],
       },
-    };
-    return obj;
+      {
+        name: "Charmander",
+        image: require("./assets/charmander.png"),
+        type: "Fire",
+        hp: 39,
+        moves: ["Scratch", "Ember", "Growl", "Leer"],
+        weaknesses: ["Water", "Rock"],
+      },
+      {
+        name: "Squirtle",
+        image: require("./assets/squirtle.png"), // Replace with the actual image path
+        type: "Water",
+        hp: 44,
+        moves: ["Tackle", "Water Gun", "Tail Whip", "Withdraw"],
+        weaknesses: ["Electric", "Grass"],
+      },
+      {
+        name: "Bulbasaur",
+        image: require("./assets/bulbasaur.png"), // Replace with the actual image path
+        type: "Grass",
+        hp: 45,
+        moves: ["Tackle", "Vine Whip", "Growl", "Leech Seed"],
+        weaknesses: ["Fire", "Ice", "Flying", "Psychic"],
+      },
+      {
+        name: "Pikachu",
+        image: require("./assets/pikachu.png"), // Replace with the actual image path
+        type: "Electric",
+        hp: 35,
+        moves: ["Quick Attack", "Thunderbolt", "Tail Whip", "Growl"],
+        weaknesses: ["Ground"],
+      },
+      {
+        name: "Charmander",
+        image: require("./assets/charmander.png"),
+        type: "Fire",
+        hp: 39,
+        moves: ["Scratch", "Ember", "Growl", "Leer"],
+        weaknesses: ["Water", "Rock"],
+      },
+      {
+        name: "Squirtle",
+        image: require("./assets/squirtle.png"), // Replace with the actual image path
+        type: "Water",
+        hp: 44,
+        moves: ["Tackle", "Water Gun", "Tail Whip", "Withdraw"],
+        weaknesses: ["Electric", "Grass"],
+      },
+      {
+        name: "Bulbasaur",
+        image: require("./assets/bulbasaur.png"), // Replace with the actual image path
+        type: "Grass",
+        hp: 45,
+        moves: ["Tackle", "Vine Whip", "Growl", "Leech Seed"],
+        weaknesses: ["Fire", "Ice", "Flying", "Psychic"],
+      },
+      {
+        name: "Pikachu",
+        image: require("./assets/pikachu.png"), // Replace with the actual image path
+        type: "Electric",
+        hp: 35,
+        moves: ["Quick Attack", "Thunderbolt", "Tail Whip", "Growl"],
+        weaknesses: ["Ground"],
+      },
+      {
+        name: "Charmander",
+        image: require("./assets/charmander.png"),
+        type: "Fire",
+        hp: 39,
+        moves: ["Scratch", "Ember", "Growl", "Leer"],
+        weaknesses: ["Water", "Rock"],
+      },
+      {
+        name: "Squirtle",
+        image: require("./assets/squirtle.png"), // Replace with the actual image path
+        type: "Water",
+        hp: 44,
+        moves: ["Tackle", "Water Gun", "Tail Whip", "Withdraw"],
+        weaknesses: ["Electric", "Grass"],
+      },
+      {
+        name: "Bulbasaur",
+        image: require("./assets/bulbasaur.png"), // Replace with the actual image path
+        type: "Grass",
+        hp: 45,
+        moves: ["Tackle", "Vine Whip", "Growl", "Leech Seed"],
+        weaknesses: ["Fire", "Ice", "Flying", "Psychic"],
+      },
+      {
+        name: "Pikachu",
+        image: require("./assets/pikachu.png"), // Replace with the actual image path
+        type: "Electric",
+        hp: 35,
+        moves: ["Quick Attack", "Thunderbolt", "Tail Whip", "Growl"],
+        weaknesses: ["Ground"],
+      },
+      {
+        name: "Charmander",
+        image: require("./assets/charmander.png"),
+        type: "Fire",
+        hp: 39,
+        moves: ["Scratch", "Ember", "Growl", "Leer"],
+        weaknesses: ["Water", "Rock"],
+      },
+      {
+        name: "Squirtle",
+        image: require("./assets/squirtle.png"), // Replace with the actual image path
+        type: "Water",
+        hp: 44,
+        moves: ["Tackle", "Water Gun", "Tail Whip", "Withdraw"],
+        weaknesses: ["Electric", "Grass"],
+      },
+      {
+        name: "Bulbasaur",
+        image: require("./assets/bulbasaur.png"), // Replace with the actual image path
+        type: "Grass",
+        hp: 45,
+        moves: ["Tackle", "Vine Whip", "Growl", "Leech Seed"],
+        weaknesses: ["Fire", "Ice", "Flying", "Psychic"],
+      },
+      {
+        name: "Pikachu",
+        image: require("./assets/pikachu.png"), // Replace with the actual image path
+        type: "Electric",
+        hp: 35,
+        moves: ["Quick Attack", "Thunderbolt", "Tail Whip", "Growl"],
+        weaknesses: ["Ground"],
+      },
+      {
+        name: "Charmander",
+        image: require("./assets/charmander.png"),
+        type: "Fire",
+        hp: 39,
+        moves: ["Scratch", "Ember", "Growl", "Leer"],
+        weaknesses: ["Water", "Rock"],
+      },
+      {
+        name: "Squirtle",
+        image: require("./assets/squirtle.png"), // Replace with the actual image path
+        type: "Water",
+        hp: 44,
+        moves: ["Tackle", "Water Gun", "Tail Whip", "Withdraw"],
+        weaknesses: ["Electric", "Grass"],
+      },
+      {
+        name: "Bulbasaur",
+        image: require("./assets/bulbasaur.png"), // Replace with the actual image path
+        type: "Grass",
+        hp: 45,
+        moves: ["Tackle", "Vine Whip", "Growl", "Leech Seed"],
+        weaknesses: ["Fire", "Ice", "Flying", "Psychic"],
+      },
+      {
+        name: "Pikachu",
+        image: require("./assets/pikachu.png"), // Replace with the actual image path
+        type: "Electric",
+        hp: 35,
+        moves: ["Quick Attack", "Thunderbolt", "Tail Whip", "Growl"],
+        weaknesses: ["Ground"],
+      },
+      {
+        name: "Charmander",
+        image: require("./assets/charmander.png"),
+        type: "Fire",
+        hp: 39,
+        moves: ["Scratch", "Ember", "Growl", "Leer"],
+        weaknesses: ["Water", "Rock"],
+      },
+      {
+        name: "Squirtle",
+        image: require("./assets/squirtle.png"), // Replace with the actual image path
+        type: "Water",
+        hp: 44,
+        moves: ["Tackle", "Water Gun", "Tail Whip", "Withdraw"],
+        weaknesses: ["Electric", "Grass"],
+      },
+      {
+        name: "Bulbasaur",
+        image: require("./assets/bulbasaur.png"), // Replace with the actual image path
+        type: "Grass",
+        hp: 45,
+        moves: ["Tackle", "Vine Whip", "Growl", "Leech Seed"],
+        weaknesses: ["Fire", "Ice", "Flying", "Psychic"],
+      },
+      {
+        name: "Pikachu",
+        image: require("./assets/pikachu.png"), // Replace with the actual image path
+        type: "Electric",
+        hp: 35,
+        moves: ["Quick Attack", "Thunderbolt", "Tail Whip", "Growl"],
+        weaknesses: ["Ground"],
+      },
+    ];
+    return arr;
   }, []);
 
   return (
     <View style={styles.container}>
       {/* <StatusBar style="auto" animated />  */}
       <SafeAreaView style={{ flex: 1 }}>
-        <ScrollView>
+        {/* <ScrollView>
           {Object.keys(chars).map((char, index) => (
             <PokemonCard key={index} {...chars[char as keyof typeof chars]} />
           ))}
-        </ScrollView>
+        </ScrollView> */}
+        <FlatList
+          data={chars}
+          renderItem={({ item, index }) => {
+            // console.log(index);
+            return <PokemonCard key={index} {...item} />;
+          }}
+          // horizontal
+          ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
+          initialNumToRender={8}
+          initialScrollIndex={4}
+        />
       </SafeAreaView>
     </View>
   );
